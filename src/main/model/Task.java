3f0d705cbb5645ae156f679e8edd3a14e6d7acd5
eddoyang <eddoyang@students.cs.ubc.ca>
2026-01-04 22:55:14 -0800

    remodeled layout

--- src/main/model/Task.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-import org.json.JSONObject;
-
-public interface Task {
-    public JSONObject toJson();
-}
--- src/main/persistence/Writable.java
+++
@@ -1,8 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-public interface Writable {
-    //returns this as JSON object
-    JSONObject toJson();
-}
